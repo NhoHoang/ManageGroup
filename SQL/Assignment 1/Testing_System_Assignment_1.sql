e01ffb963d8639ed2dffed330edd4d418441c807
NhoHoang <nhoht97@gmail.com>
2021-07-31 20:14:52 +0700

    Updating Test System Assignment 1

--- SQL/Assignment 1/Testing_System_Assignment_1.sql
+++ SQL/Assignment 1/Testing_System_Assignment_1.sql
@@ -1,81 +1,82 @@
-CREATE DATABASE IF NOT EXISTS `testing_system_assignment_1`;
-USE `testing_system_assignment_1`;
+CREATE DATABASE IF NOT EXISTS `Testing_System_Assignment_1`;
+USE `Testing_System_Assignment_1`;
 
-
-CREATE TABLE IF NOT EXISTS `department`(
-department_ID int,
-department_name varchar(50)
+CREATE TABLE IF NOT EXISTS `Department`(
+	DepartmentID 	int,
+	DepartmentName 	varchar(50)
 );
 CREATE TABLE IF NOT EXISTS `Positon`(
-Position_ID int,
-Positon_name varchar(50)
+	PositionID 		int,
+	PositonName 	varchar(50)
 );
 
 CREATE TABLE IF NOT EXISTS `Account`(
-AccountID int,
-Email varchar(50),
-username varchar(50),
-fullname varchar(50),
-departmentID int,
-positionID int,
-createdate date
+	AccountID 		int,
+	Email 			varchar(50),
+	Username		varchar(50),
+	Fullname 		varchar(50),
+	DepartmentID 	int,
+	PositionID 		int,
+	Createdate 		date
 );
  
 
-CREATE TABLE IF NOT EXISTS `group`(
-groupID int,
-groupname varchar(50),
-creatorID int,
-createdate date
+CREATE TABLE IF NOT EXISTS `Group`(
+	GroupID 		int,
+	Groupname 		varchar(50),
+	CreatorID 		int,
+	Createdate 		date
 );
 
-CREATE TABLE IF NOT EXISTS `groupaccount`(
-groupID int,
-accountid int,
-joindate date
+CREATE TABLE IF NOT EXISTS `Groupaccount`(
+	GroupID 		int,
+	Accountid 		int,
+	Joindate 		date
 );
 
-CREATE TABLE IF NOT EXISTS`typequestion`(
-TypeID int,
-typename varchar(50)
-);
-
-
-CREATE TABLE IF NOT EXISTS `categoryquestion`(
-categoryID int,
-categoryname varchar(50)
+CREATE TABLE IF NOT EXISTS`Typequestion`(
+	TypeID 			int,
+	Typename 		varchar(50)
 );
 
 
-CREATE TABLE IF NOT EXISTS `question`(
-questionID int,
-content varchar(1500),
-categoryid int,
-typeid int,
-creatorid int,
-creatordate date
+CREATE TABLE IF NOT EXISTS `Categoryquestion`(
+	CategoryID 		int,
+	CategoryName 	varchar(50)
 );
 
 
-CREATE TABLE IF NOT EXISTS `answer`(
-answerID int,
-content varchar(1500),
-questionid int,
-iscorrect varchar(50)
+CREATE TABLE IF NOT EXISTS `Question`(
+	QuestionID 		int,
+	Content 		varchar(1500),
+	CategoryID 		int,
+	TypeID 			int,
+	CreatorID 		int,
+	CreatorDate 	date
 );
 
-CREATE TABLE IF NOT EXISTS `exam`(
-examID int,
-`code` varchar(50),
-title varchar(50),
-categoryID int,
-duration time,
-creatorID int,
-createdate date
+
+CREATE TABLE IF NOT EXISTS `Answer`(
+	AnswerID 		int,
+	Content 		varchar(1500),
+	Questionid 		int,
+	Iscorrect 		varchar(50)
 );
 
-CREATE TABLE IF NOT EXISTS `examquestion`(
-examID int,
-questionID int
+CREATE TABLE IF NOT EXISTS `Exam`(
+	ExamID 			int,
+	`code` 			varchar(50),
+	Title 			varchar(50),
+	CategoryID 		int,
+	Duration 		time,
+	CreatorID 		int,
+	Createdate 		date
 );
--- update
+
+CREATE TABLE IF NOT EXISTS `Examquestion`(
+	ExamID 			int,
+	QuestionID 		int
+);
+
+
+-- updated
