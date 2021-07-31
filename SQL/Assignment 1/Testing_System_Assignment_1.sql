64c54a0ff141ea8ddd8bf46b6955d21a3b5b49a4
NhoHoang <nhoht97@gmail.com>
2021-07-31 13:17:56 +0700

    Subimit asignment 1

---
+++ SQL/Assignment 1/Testing_System_Assignment_1.sql
@@ -0,0 +1,67 @@
+CREATE DATABASE if not exists `testing_system_assignment_1`;
+use `testing_system_assignment_1`;
+
+
+drop table if exists `department`;
+CREATE table if not exists `department`(
+department_ID int,
+department_name varchar(50)
+);
+create table if not exists `Positon`(
+Position_ID int,
+Positon_name varchar(50)
+);
+
+create table if not exists `Account`(
+AccountID int,
+Email varchar(50),
+username varchar(50),
+fullname varchar(50),
+departmentID int,
+positionID int,
+createdate date
+);
+ 
+
+create table if not exists `group`(
+groupID int,
+groupname varchar(50),
+creatorID int,
+createdate date
+);
+
+create table if not exists `groupaccount`(
+groupID int,
+accountid int,
+joindate date
+);
+
+create table if not exists `typequestion`(
+TypeID int,
+typename varchar(50)
+);
+
+
+create table if not exists `categoryquestion`(
+categoryID int,
+categoryname varchar(50)
+);
+
+
+create table if not exists `question`(
+questionID int,
+content varchar(1500),
+categoryid int,
+typeid int,
+creatorid int,
+creatordate date
+
+);
+
+
+create table if not exists `answer`(
+answerID int,
+content varchar(1500),
+questionid int,
+iscorrect varchar(50)
+);
